497f2e49b815ade539ba96b40c7887018db74ee4
caiocsgomes <caiocsgomes@gmail.com>
2024-01-09 20:12:42 +0200

    first commit

---
+++ terraform/backend/main.tf
@@ -0,0 +1,35 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "sa-east-1"
+}
+
+resource "aws_s3_bucket" "s3_backend_bucket" {
+  bucket_prefix =  = "${var.project_name}-tf-backend-"
+}
+
+resource "aws_s3_bucket_versioning" "s3_backend_bucket_versioning" {
+  bucket = aws_s3_bucket.s3_backend_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "dybanodb_backend" {
+  name           = "${var.project_name}-tf-backend"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
