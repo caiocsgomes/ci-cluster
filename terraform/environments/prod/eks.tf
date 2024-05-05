588552a1b5c15d137affcc8b0a9b1b1872bdc0d6
Caio Gomes <caio.gomes@transferwise.com>
2024-05-05 11:45:22 -0300

    Adding profile verification apply workflow

--- terraform/environments/prod/eks.tf
+++ terraform/environments/prod/eks.tf
@@ -15,10 +15,6 @@ data "aws_eks_cluster_auth" "default" {
   name = module.eks.cluster_id
 }
 
-data "aws_eks_cluster" "default" {
-  name = module.eks.cluster_name
-}
-
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
