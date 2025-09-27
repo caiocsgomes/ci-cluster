3e0207b794ec53475942b60f92f1bddb3fd54758
caiocsgomes <caiocsgomes@gmail.com>
2025-09-27 12:52:08 +0300

    chore: changing name of cluster

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 21.0"
 
-  name               = "${local.name}-al2023"
+  name               = local.name
   kubernetes_version = local.cluster_version
 
   # EKS Addons
